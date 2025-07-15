6a5d6f90e7097c95d9bbd279b7fa6e13fd5f6d01
jonggil.kim <jonggil.kim@dkbmc.com>
2025-07-15 16:21:13 +0900

    구조 수정

--- plugin.rb
+++ plugin.rb
@@ -13,21 +13,21 @@ after_initialize do
   add_admin_route("js.manual_points.title", "manual-points", { use_new_show_route: true })
 
   Discourse::Application.routes.append do
-    get '/admin/plugins/manual-points' => 'admin/plugins#index', constraints: StaffConstraint.new
+    get '/admin/plugins/manual-points' => 'admin/plugins#index'
   end
 
-  module ::DiscoursePointAssign
-    class Engine < ::Rails::Engine
-      engine_name PLUGIN_NAME
-      isolate_namespace DiscoursePointAssign
-    end
-  end
+  # module ::DiscoursePointAssign
+  #   class Engine < ::Rails::Engine
+  #     engine_name PLUGIN_NAME
+  #     isolate_namespace DiscoursePointAssign
+  #   end
+  # end
 
-  DiscoursePointAssign::Engine.routes.draw do
-    post "/" => "manual_points#create"
-  end
+  # DiscoursePointAssign::Engine.routes.draw do
+  #   post "/" => "manual_points#create"
+  # end
 
-  Discourse::Application.routes.append do
-    mount ::DiscoursePointAssign::Engine, at: "/admin/plugins/manual-points/api"
-  end
+  # Discourse::Application.routes.append do
+  #   mount ::DiscoursePointAssign::Engine, at: "/admin/plugins/manual-points/api"
+  # end
 end
